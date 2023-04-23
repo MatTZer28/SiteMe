b588e8e4a9e85e533322a57e6f08f1dc3b5f78ec
MattZhang <mattzhang@owlsome.tech>
2023-04-23 15:43:26 +0800

    add subtitle and title fly out animation

--- script/subtitles.js
+++ script/subtitles.js
@@ -1,6 +1,9 @@
 const HOVER_LISTENER_DELAY = 5.6;
+const FLY_OUT_ANIMATION_DURATION = 2;
 
 const subtitles = document.getElementById("subtitles-sect").childNodes;
+const subsect = document.getElementById("subtitles-sect");
+const title = document.getElementById("home-title");
 
 const HOVER_CSS_STYLE = `
 .subtitle:hover {
@@ -42,6 +45,30 @@ const HOVER_CSS_STYLE = `
 }
 `
 
+const FLY_OUT_CSS_STYLE = `
+#subtitles-sect {
+    animation: subtitle-fly-out ${FLY_OUT_ANIMATION_DURATION}s forwards;
+    animation-timing-function: cubic-bezier(.77,-0.01,.11,1);
+}
+
+@keyframes subtitle-fly-out {
+    100% {
+        transform: translateY(-50vh)
+    }
+}
+
+#home-title {
+    animation: title-fly-out ${FLY_OUT_ANIMATION_DURATION}s forwards;
+    animation-timing-function: cubic-bezier(.77,-0.01,.11,1);
+}
+
+@keyframes title-fly-out {
+    100% {
+        transform: translateY(-100vh)
+    }
+}
+`
+
 function handleHover() {
     setTimeout(() => {
         addHoverAnimation();
@@ -59,8 +86,27 @@ function initHoverEventListener() {
     for (elem of subtitles) {
         if (!(elem instanceof HTMLDivElement)) continue;
         elem.firstChild.addEventListener("click", (event) => {
+            animateFlyOut();
         });
     }
+}
+
+function animateFlyOut() {
+    addFlyOutAnimation();
+    removeAfterFlyOut();
+}
+
+function addFlyOutAnimation() {
+    let styleSheet = document.createElement("style");
+    styleSheet.innerText = FLY_OUT_CSS_STYLE;
+    document.head.appendChild(styleSheet);
+}
+
+function removeAfterFlyOut() {
+    setTimeout(() => {
+        subsect.remove();
+        title.remove();
+    }, FLY_OUT_ANIMATION_DURATION * 1000);
 }
 
 document.addEventListener("DOMContentLoaded", handleHover);
